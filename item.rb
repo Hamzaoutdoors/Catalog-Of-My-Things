4f6487e51d5aaa4702e362ee25f967768f126b72
Carlos E. Feliz <74747182+xJokeer@users.noreply.github.com>
2021-11-23 12:35:01 -0400

    Update methods and add private method

--- item.rb
+++ item.rb
@@ -6,23 +6,25 @@ class Item
 
   attr_accessor :genre, :author, :source, :label, :publish_date, :archived
 
-  def initialize(publish_date, archived: false)
+  def initialize(publish_date)
     @id = Random.rand(1..10_000)
-    @genre = genre
-    @author = author
-    @source = source
-    @label = label
+    @genre = nil
+    @author = nil
+    @source = nil
+    @label = nil
     @publish_date = publish_date
-    @archived = archived
-  end
-
-  def can_be_archived?
-    archived_date = Date.iso8601(@publish_date).next_year(10)
-    Date.today > archived_date
+    @archived = false
   end
 
   def move_to_archive
     @archived = can_be_archived?
+  end
+
+  private
+
+  def can_be_archived?
+    archived_date = Date.iso8601(@publish_date).next_year(10)
+    Date.today > archived_date
   end
 end
 
