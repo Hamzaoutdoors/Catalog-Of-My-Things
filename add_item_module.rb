cab120204f6d570e28b9f84228a6964d172d395c
Carlos E. Feliz <74747182+xJokeer@users.noreply.github.com>
2021-11-24 13:23:50 -0400

    add options for book

--- add_item_module.rb
+++ add_item_module.rb
@@ -1,4 +1,5 @@
 require './music_album'
+require './book'
 require 'date'
 
 module ItemIntializer
@@ -86,8 +87,41 @@ module ItemIntializer
     end
   end
 
+  def book_album_info
+    print 'Published date (yyyy-mm-dd): '
+    date_answer = gets.chomp
+    publish_date = validate_date(date_answer)
+
+    print 'Is it good or bad? [good/bad]: '
+
+    answer = gets.chomp.downcase
+    cover_state = cover_state?(answer)
+
+    print 'Who is the publisher? '
+    publisher_answer = gets.chomp.downcase
+    publisher = publisher_answer
+
+    puts 'Book created successfully ✔️'
+    [publish_date, publisher, cover_state]
+  end
+
   def create_book
-    puts 'book'
+    publish_date, publisher, cover_state = book_album_info
+    book_album = Book.new(publish_date, publisher, cover_state)
+    @books.push(book_album)
+  end
+
+  def cover_state?(answer)
+    case answer
+    when 'bad'
+      false
+    when 'good'
+      true
+    else
+      print 'Could you please specify your answer by [good/bad]: '
+      new_answer = gets.chomp
+      cover_state?(new_answer)
+    end
   end
 
   def create_game
