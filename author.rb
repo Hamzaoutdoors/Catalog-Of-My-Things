06e846c85457b071c6600f161657b4c13e5eca54
rotimiazeez <harzeyzhrotimi@gmail.com>
2021-11-24 07:22:14 -0800

    feat: Add author class

---
+++ author.rb
@@ -0,0 +1,15 @@
+class Author
+  attr_accessor :first_name, :last_name, :items
+
+  def initialize(first_name, last_name)
+    @id = Random.rand(0..10_000)
+    @first_name = first_name
+    @last_name = last_name
+    @items = []
+  end
+
+  def add_item(item)
+    @items.push(item)
+    item.author = self
+  end
+end
