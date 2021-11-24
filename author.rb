8f3ba6bebe4d0afc336bc26c89f7d98207618e78
rotimiazeez <harzeyzhrotimi@gmail.com>
2021-11-24 09:52:42 -0800

    fix: Fix linter error

--- author.rb
+++ author.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Create class Author
 class Author
   attr_accessor :first_name, :last_name, :items
 
--- game.rb
+++ game.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
+
 require_relative 'item'
 
+# Create class Game
 class Game < Item
   def initialize(publish_date, multiplayer, last_played_at)
     super(publish_date)
@@ -8,6 +11,7 @@ class Game < Item
   end
 
   private
+
   def can_be_archived?
     Date.today > Date.iso8601(@last_played_at).next_year(2) && super
   end
