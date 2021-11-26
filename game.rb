c8b92251e7a1d360efcb045e9f111969edc5e227
rotimiazeez <harzeyzhrotimi@gmail.com>
2021-11-26 03:28:00 -0800

    feat: Add attr_accessor method to game

--- game.rb
+++ game.rb
@@ -4,7 +4,10 @@ require_relative 'item'
 
 # Create class Game
 class Game < Item
-  def initialize(publish_date, multiplayer, last_played_at)
+  attr_accessor :last_played_at, :multiplayer
+  attr_reader :publish_date
+
+  def initialize(last_played_at, multiplayer, publish_date)
     super(publish_date)
     @multiplayer = multiplayer
     @last_played_at = last_played_at
